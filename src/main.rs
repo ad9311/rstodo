53f200b77dfa6b815003d2d345b716348c7c89a1
Ángel Díaz <diazangel9311@gmail.com>
2023-03-29 00:13:55 -0500

    Implement loop to create a list of tasks

--- src/main.rs
+++ src/main.rs
@@ -6,22 +6,25 @@ use todo::*;
 
 fn main() {
     print_app_name();
-
-    print_input_title();
-    let title = read_title();
-    clear_output();
-
-    print_input_priority();
-    let priority = read_priority();
-    clear_output();
-
-    let task = Task::from(title, Priority::from(priority));
-
     let mut tasks: Vec<Task> = Vec::new();
 
-    tasks.push(task);
+    loop {
+        print_input_title();
+        let title = read_title();
+        clear_output();
+        print_app_name();
 
-    for task in &tasks {
-        println!("{}", task.show())
+        print_input_priority();
+        let priority = read_priority();
+        clear_output();
+        print_app_name();
+
+        let task = Task::from(title, Priority::from(priority));
+
+        tasks.push(task);
+
+        for task in &tasks {
+            println!("{}", task.show())
+        }
     }
 }
